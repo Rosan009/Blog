8bbc06a14b457d7056502c40a9c6760f7b5717b8
Rosan009 <rosannarwal4@gmail.com>
2025-04-04 17:55:41 +0530

    Initial commit

---
+++ Blog-BackEnd/src/main/java/com/omnify/Blog_BackEnd/BlogBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.omnify.Blog_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackEndApplication.class, args);
+	}
+
+}
---
+++ Blog-BackEnd/src/test/java/com/omnify/Blog_BackEnd/BlogBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnify.Blog_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
